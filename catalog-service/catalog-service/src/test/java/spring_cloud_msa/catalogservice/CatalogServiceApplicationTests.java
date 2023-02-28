aba7a9d19799b5d86b1e688154588495857899f5
liveforone <yc4852@gmail.com>
2023-02-28 20:30:17 +0900

    Add catalog service.

---
+++ catalog-service/catalog-service/src/test/java/spring_cloud_msa/catalogservice/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_cloud_msa.catalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
