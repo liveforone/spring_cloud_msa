a075cf48a21c8ae7299d68d56fb77deff3179b8a
liveforone <yc4852@gmail.com>
2023-02-28 19:01:24 +0900

    Add feign client and discovery client.

--- user-service/user-service/src/main/java/spring_cloud_msa/userservice/UserServiceApplication.java
+++ user-service/user-service/src/main/java/spring_cloud_msa/userservice/UserServiceApplication.java
@@ -1,7 +1,11 @@
 package spring_cloud_msa.userservice;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +16,8 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Bean
+	public Logger.Level feignLoggerLever() {
+		return Logger.Level.FULL;
+	}
 }
