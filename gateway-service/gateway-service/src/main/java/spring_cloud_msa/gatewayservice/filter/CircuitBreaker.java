0d6ccdb441d2e212508b7ed94b33b34473645c02
liveforone <yc4852@gmail.com>
2023-02-28 17:45:19 +0900

    Add CircuitBreaker.

---
+++ gateway-service/gateway-service/src/main/java/spring_cloud_msa/gatewayservice/filter/CircuitBreaker.java
@@ -0,0 +1,27 @@
+package spring_cloud_msa.gatewayservice.filter;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+public class CircuitBreaker {
+
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        TimeLimiterConfig timeLimiterConfig = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(4))
+                .build();
+
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(timeLimiterConfig)
+                .build());
+    }
+}
