a3190a7f8465c45ade6eda8f62c8844ffd358b2b
liveforone <yc4852@gmail.com>
2023-02-28 18:07:17 +0900

    Add user service.

---
+++ user-service/user-service/src/main/java/spring_cloud_msa/userservice/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package spring_cloud_msa.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
