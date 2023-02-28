68a76431f2775bf8b9d312d0ada28af8c40b59d8
liveforone <yc4852@gmail.com>
2023-02-28 17:25:09 +0900

    Initial commit

---
+++ discovery-service/discovery-service/src/test/java/spring_cloud_msa/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_cloud_msa.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
