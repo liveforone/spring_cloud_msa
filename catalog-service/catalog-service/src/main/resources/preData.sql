2f11adf18ddbf10e734c9dd031ea85cc409ea0f4
liveforone <yc4852@gmail.com>
2023-02-28 21:07:42 +0900

    Add pre data.

---
+++ catalog-service/catalog-service/src/main/resources/preData.sql
@@ -0,0 +1,3 @@
+insert into catalog(product_id, product_name, stock, unit_price) values ('001', 'tokyo', 100, 1500);
+insert into catalog(product_id, product_name, stock, unit_price) values ('002', 'seoul', 200, 2000);
+insert into catalog(product_id, product_name, stock, unit_price) values ('003', 'london', 300, 3000);
