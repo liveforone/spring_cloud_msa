ed7e9a4aa95fe2da24fa1be5ce09c7073ddc5d24
liveforone <yc4852@gmail.com>
2023-02-28 17:35:54 +0900

    Initial gateway service.

---
+++ gateway-service/gateway-service/src/main/java/spring_cloud_msa/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,20 @@
+package spring_cloud_msa.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	public HttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
+}
---
+++ gateway-service/gateway-service/src/test/java/spring_cloud_msa/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_cloud_msa.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
